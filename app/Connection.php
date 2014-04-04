49fbe67ed6b22e282653706059a49e3ae2226b8d
Vicente Martins <geo.martins7@gmail.com>
2014-04-03 23:23:28 -0300

    Conexão usando Singleton extendendo PDO.

--- app/Connection.php
+++ app/Connection.php
@@ -1,7 +1,29 @@
 <?php
+class Connection extends PDO {
+    
+    const DNS = 'mysql:host=localhost; dbname=prefered';
+    const USER = 'root';
+    const PWD = '123456';
+    
+    private static $conn;
 
-class Connection {
-    public function getConnection(){
-        return new PDO('mysql:host=localhost;dbname=prefered', 'root', '123456');
+    protected function Connection($dns,$user, $password){
+        try {
+            self::$conn = parent::__construct(self::DNS, self::USER, self::PWD);
+        } catch (PDOException $ex) {
+            echo "Connection fail: ".$ex->getMessage();
+            return FALSE;
+        }
+    }
+    
+    public function __destruct() {
+        self::$conn = FALSE;        
+    }
+    
+    public static function getConnection() {
+        if(self::$conn == NULL){
+            self::$conn = new Connection(self::DNS, self::USER, self::PWD);
+        }
+        return self::$conn;
     }
 }
